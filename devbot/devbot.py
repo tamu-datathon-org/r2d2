78e5ad1f3fa3d414a2722e61fa0a0ba134c4ebc8
phulsechinmay <phulsechinmay@gmail.com>
2020-07-22 16:15:14 -0700

    feat: Added error logging endpoint to devbot

--- devbot/devbot.py
+++ devbot/devbot.py
@@ -5,16 +5,19 @@ from flask import Flask, request, make_response
 app = Flask(__name__)
 app.config.from_pyfile("settings.py")
 
+# Need to import utils after intializing Flask app
+from utils import is_authorized_request
+
 slack_client = WebClient(token=app.config.get("SLACK_TOKEN"))
 
 
 @app.route("/slack/get-applicants", methods=["POST"])
-def command():
+def get_applicants_csv():
+    request_info = request.form
+    headers = {
+        "Gatekeeper-Integration": app.config.get("GATEKEEPER_INTEGRATION_SECRET")
+    }
     try:
-        request_info = request.form
-        headers = {
-            "Gatekeeper-Integration": app.config.get("GATEKEEPER_INTEGRATION_SECRET")
-        }
         applicant_data = requests.get(
             f"{app.config.get('OBOS_URL')}/application/all", headers=headers)
         if (applicant_data.status_code != 200):
@@ -30,6 +33,26 @@ def command():
         return make_response("", 200)
     except Exception as e:
         return make_response(f"Something went wrong, here's what I know: {e}", 200)
+
+
+@app.route("/slack/log-error", methods=["POST"])
+def log_error():
+    if not is_authorized_request(request):
+        return make_response(f"Invalid Gatekeeper Integration header", 400)
+    if "error" not in request.json:
+        return make_response(f"Post body must contain error field", 400)
+        
+    error_msg = request.json["error"]
+    source = request.json["source"] if "source" in request.json else ""
+    try:
+        error_text = f"ERROR [{source}]:\n\n{error_msg}"
+        slack_client.chat_postMessage(
+            channel=app.config.get("SLACK_ERROR_LOG_CHANNEL_ID"),
+            text=error_text
+        )
+    except Exception as e:
+        print(e)
+    return make_response("")
 
 
 if __name__ == "__main__":
